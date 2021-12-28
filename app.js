d69b1798c99c034da2ce7fc79d0fb539f6e1f55e
TJ Lawless <87883465+tommyjohnny36@users.noreply.github.com>
2021-12-28 09:39:42 -0500

    Updated app.js functions

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-d3.json("samples.json").then((data) => {
-  let samples = data.samples;
-  let result = samples[0];
+function init() {
 
-  let otu_ids = result.otu_ids
+  d3.json("samples.json").then((data) => {
+    let samples = data.samples;
+    let result = samples[0];
 
-  
-  let sample_values = Object.values(result.sample_values)
-  let otu_ids = Object.keys(result.otu_ids)
+    let otu_ids = result.otu_ids
+    console.log(otu_ids)
+    
+    let sample_values = Object.values(result.sample_values)
+  // let otu_ids = Object.keys(result.otu_ids)
 
 // Create an array that pulls in sample_values as the values to be graphed
 // let sample_values = Object.values(result.sample_values)
@@ -16,25 +18,43 @@ d3.json("samples.json").then((data) => {
 // console.log(sample_values)
 
   // Display the default plot
-function init() {
+
+
+  let dropDown = d3.select("#selDataset")
+
+  let metaData = data.metadata
+  metaData.forEach((d) => {
+    dropDown.append("option").text(d.id).property("value", d.id)
+
+  })
+
+// // The new student and grade to add to the table
+// var newGrade = ["Wash", 79];
+
+// // Use D3 to select the table
+// var table = d3.select("table");
+
+// // Use d3 to create a bootstrap striped table
+// // http://getbootstrap.com/docs/3.3/css/#tables-striped
+// table.attr("class", "table table-striped");
+
+// // Use D3 to select the table body
+// var tbody = d3.select("tbody");
+
+// // Append one table row `tr` to the table body
+// var row = tbody.append("tr");
+
+// // Append one cell for the student name
+// row.append("td").text(newGrade[0]);
+
+// // Append one cell for the student grade
+// row.append("td").text(newGrade[1]);
+
+
   // Fetch the JSON data and console log it
 
-  let data = [{
-  values: sample_values,
-  labels: otu_ids,
-  type: "bar"
-}];
 
-    let layout = {
-      height: 600,
-      width: 800
-    };
 
-    Plotly.newPlot("bar", data, layout);
-  
-})
-
-init();
 // // Create an array of each country's numbers
 // var us = Object.values(data.us);
 // var uk = Object.values(data.uk);
@@ -59,34 +79,43 @@ init();
 //   Plotly.newPlot("pie", data, layout);
 // }
 
-// // On change to the DOM, call getData()
-// d3.selectAll("#selDataset").on("change", getData);
+})}
 
-// // Function called by DOM changes
-// function getData() {
-//   var dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a variable
-//   var dataset = dropdownMenu.property("value");
-//   // Initialize an empty array for the country's data
-//   var data = [];
+function createDemographicData(sampleId){
+  d3.json("samples.json").then((data) => {
+  let metaData = data.metadata
+  let filteredData = metaData.filter(d => d.id == sampleId)
+  console.log(filteredData)
+  let sampleMetaData = d3.select("#sample-metadata")
+  Object.entries(filteredData).forEach(([key, value]) => {
+    sampleMetaData.append("h4").text(`${value}`);
+  })
+  // metaData.forEach((d) => {
+  // dropDown.append("option").text(d.id).property("value", d.id)
 
-//   if (dataset == 'us') {
-//       data = us;
-//   }
-//   else if (dataset == 'uk') {
-//       data = uk;
-//   }
-//   else if (dataset == 'canada') {
-//       data = canada;
-//   }
-//   // Call function to update the chart
-//   updatePlotly(data);
-// }
+  // }
+  })
 
-// // Update the restyled plot's values
-// function updatePlotly(newdata) {
-//   Plotly.restyle("pie", "values", [newdata]);
-// }
-
-// init();
 }
+
+function optionChanged(sampleId){
+  createDemographicData(sampleId)
+}
+
+function createPlots() {
+
+  let chartData = [{
+    values: sample_values,
+    labels: otu_ids,
+    type: "bar"
+  }];
+
+      let layout = {
+        height: 600,
+        width: 800
+      };
+
+      Plotly.newPlot("bar", chartData, layout);
+  
+}
+init();
