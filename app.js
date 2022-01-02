a6f66a5269504f6a7f7fc3630deea0abbb1e0c76
TJ Lawless <87883465+tommyjohnny36@users.noreply.github.com>
2022-01-01 21:30:48 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,121 +1,115 @@
-function init() {
+const sampleMetaData = d3.select("#sample-metadata");
 
-  d3.json("samples.json").then((data) => {
-    let samples = data.samples;
-    let result = samples[0];
+// var layout = {
+//   width: "100%",
+//   height: "100%"
+// }
+// var width = "100%"
+// var height = "100%"
 
-    let otu_ids = result.otu_ids
+// const svg = sampleMetaData.append("svg")
+//                           .attr('width', width)
+//                           .attr('height', height)
+
+// Parse JSON data
+
+d3.json("samples.json")
+  .then(data =>{
+
+
+    // Create a horizontal bar chart 
+    // with a dropdown menu to display the top 10 OTUs found in that individual.
+
+    const getSamplesData = data.samples;
+    let sampleResults = getSamplesData[0];
+
+    const getMetaData = data.metadata;
+    let metaDataResults= getMetaData[0];
+
+
+    let otu_ids = sampleResults.otu_ids
     console.log(otu_ids)
+
+
+  // let dropDown = d3.select("#selDataset")
+
+  
+  // metaData.forEach((d) => {
+  //   dropDown.append("option").text(d.id).property("value", d.id)
+
+  //     // Filter metadata so that only the id is returned 
+  //     // when equality is found between the function value sampleId and 
+  //     // id from metadata object
+  //     let filteredData = data.filter(d => d.id == sampleId)
+  //     console.log(filteredData)
     
-    let sample_values = Object.values(result.sample_values)
-  // let otu_ids = Object.keys(result.otu_ids)
-
-// Create an array that pulls in sample_values as the values to be graphed
-// let sample_values = Object.values(result.sample_values)
-
-// Create an array of chart labels
-// let otu_labels = Object.keys(result.otu_labels)
-// console.log(sample_values)
-
-  // Display the default plot
-
-
-  let dropDown = d3.select("#selDataset")
-
-  let metaData = data.metadata
-  metaData.forEach((d) => {
-    dropDown.append("option").text(d.id).property("value", d.id)
-
   })
+//     function createDemographicData(sampleId){
 
-// // The new student and grade to add to the table
-// var newGrade = ["Wash", 79];
+//       // Helper variable to call metadata object
+//       let metaData = data.metadata
+    
+ 
+//       // Select the #sample-metadata <div> 'id' tag within the index.html file
+//       // and take the entry from filteredData, and append the information as text 
+//       // forEach key and value inside the container
+    
+//       let sampleMetaData = d3.selectAll("#sample-metadata")
+    
+//       let dem_table = Object.entries(filteredData).forEach(([key, value]) => {
+//         sampleMetaData.append("h4").text(key,value);
+//       })
+//       console.log(dem_table)
+//       // Object.entries(filteredData).forEach(([key, value]) => {
+//       //   sampleMetaData.append("h4").text(`${key, value}`);
+      
+//       })
+    
+//     }
+    
+//     // data is exists and I now begin to put it all together
 
-// // Use D3 to select the table
-// var table = d3.select("table");
+//     const bubble = svg.selectAll("bubble")
+//           .data(data.metadata);
 
-// // Use d3 to create a bootstrap striped table
-// // http://getbootstrap.com/docs/3.3/css/#tables-striped
-// table.attr("class", "table table-striped");
+//     bubble.attr("cx", (d, i) => Math.floor(Math.random() * 200) + d.samples.otu_ids*i)
+//           .attr("cy", (d, i) => Math.floor(Math.random() * 100) + d.samples.sample_values)
+//           .attr("r", (d, i) => (d.samples.sample_values) * 2)
+//           .attr("fill", "black")
 
-// // Use D3 to select the table body
-// var tbody = d3.select("tbody");
-
-// // Append one table row `tr` to the table body
-// var row = tbody.append("tr");
-
-// // Append one cell for the student name
-// row.append("td").text(newGrade[0]);
-
-// // Append one cell for the student grade
-// row.append("td").text(newGrade[1]);
+//     // append enter selection to add new bubbles to the html area
+//     bubble.enter()
+//           .append("bubble")
+//           .attr("cx", (d, i) => Math.floor(Math.random() * 200) + d.samples.otu_ids*i)
+//           .attr("cy", (d, i) => Math.floor(Math.random() * 100) + d.samples.sample_values)
+//           .attr("r", (d, i) => (d.samples.sample_values) * 2)
+//           .attr("fill", "black")
 
 
-  // Fetch the JSON data and console log it
-
-
-
-// // Create an array of each country's numbers
-// var us = Object.values(data.us);
-// var uk = Object.values(data.uk);
-// var canada = Object.values(data.canada);
-
-// // Create an array of music provider labels
-// var labels = Object.keys(data.us);
-
-// // Display the default plot
 // function init() {
-//   var data = [{
-//     values: us,
-//     labels: labels,
-//     type: "pie"
-//   }];
 
-//   var layout = {
-//     height: 600,
-//     width: 800
-//   };
+//   })
 
-//   Plotly.newPlot("pie", data, layout);
+// })}
+
+// function optionChanged(sampleId){
+//   createDemographicData(sampleId)
 // }
 
-})}
+// function createPlots() {
 
-function createDemographicData(sampleId){
-  d3.json("samples.json").then((data) => {
-  let metaData = data.metadata
-  let filteredData = metaData.filter(d => d.id == sampleId)
-  console.log(filteredData)
-  let sampleMetaData = d3.select("#sample-metadata")
-  Object.entries(filteredData).forEach(([key, value]) => {
-    sampleMetaData.append("h4").text(`${value}`);
-  })
-  // metaData.forEach((d) => {
-  // dropDown.append("option").text(d.id).property("value", d.id)
+//   let chartData = [{
+//     values: sample_values,
+//     labels: otu_ids,
+//     type: "bar"
+//   }];
 
-  // }
-  })
+//       let layout = {
+//         height: 600,
+//         width: 800
+//       };
 
-}
-
-function optionChanged(sampleId){
-  createDemographicData(sampleId)
-}
-
-function createPlots() {
-
-  let chartData = [{
-    values: sample_values,
-    labels: otu_ids,
-    type: "bar"
-  }];
-
-      let layout = {
-        height: 600,
-        width: 800
-      };
-
-      Plotly.newPlot("bar", chartData, layout);
+//       Plotly.newPlot("bar", chartData, layout);
   
-}
-init();
+// }
+// init();
